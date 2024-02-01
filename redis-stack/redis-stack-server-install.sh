0fead37e34fc30994512a16636740fd6377d99b2
robertfeliciano <robertfeliciano314@gmail.com>
2024-02-01 15:06:27 -0500

    better

--- redis-stack/redis-stack-server-install.sh
+++ redis-stack/redis-stack-server-install.sh
@@ -1,10 +1,7 @@
-# requires redis to be installed first. this is so we can use the redis
-# config file with redis-stack-server and also to set the user/group below 
-# as redis
-
 curl https://packages.redis.io/redis-stack/redis-stack-server-7.2.0-v8-x86_64.AppImage -o redis-stack-server
 mv redis-stack-server /usr/local/bin/redis-stack-server
 mkdir /var/lib/redis-stack
+mv redis-stack.conf /etc/redis-stack.conf
 
 cat <<EOF > /etc/systemd/system/redis-stack-server.service
 # /etc/systemd/system/redis-stack-server.service
@@ -25,10 +22,9 @@ WantedBy=multi-user.target
 EOF
 
 systemctl daemon-reload
-/sbin/restorecon -v /etc/systemd/system/redis-stack-server.service 
+/sbin/restorecon -v /etc/systemd/system/redis-stack-server.service
 chcon -t bin_t /usr/local/bin/redis-stack-server
 systemctl daemon-reload
-systemd-analyze verify redis-stack-server.service
 systemctl enable redis-stack-server.service
 systemctl start redis-stack-server
 systemctl status redis-stack-server.service
